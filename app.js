7d37af781f43793d7ce8e529a5b6ed95bc13da98
Daniel Lucas <githubb375@gmail.com>
2022-05-12 09:52:31 -0400

    teste

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log('linha 0');
+// console.log('linha 1');
+// console.log('linha 2');
+// console.log('linha 3');
+// console.log('linha 4');
+// console.log('linha 5');
+
+// i - index
+const frutas = ['genipapo', 'uva', 'manga'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
